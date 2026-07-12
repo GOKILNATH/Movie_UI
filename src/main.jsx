5cef5eea4e200ae3263f5147fc1c1c7387f7f182
gokul j <GOKILNATH>
2026-07-12 21:28:55 +0530

    Fix deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { HashRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
+import { BrowserRouter} from 'react-router-dom';
+import  ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 )
